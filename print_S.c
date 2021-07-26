064c8260e5fee53dc9f0480732cc4d1b42c7e2fa
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-07-26 14:49:47 +0000

    fix 7

--- print_S.c
+++ print_S.c
@@ -10,26 +10,20 @@ int print_S(va_list list)
 	int count = 0, i = 0;
 	char *str = va_arg(list, char *);
 	char c;
-	char range[17] = "0123456789ABCDEF";
-	char *s;
 
 	while (str[i])
 	{
 		c = str[i];
 		if ((c > 0 && c < 32) || c >= 127)
 		{
-			_putchar('\\');
-			_putchar('x');
-			_putchar('0');
-			count += 3;
-			s = convert_num_to_base(range, (unsigned int)c, 16);
-			_puts(s);
-			count += _strlen(s);
+			count += _putchar('\\');
+			count += _putchar('x');
+			count += _putchar('0');
+			count += printf("%X", (unsigned int) c);
 		}
 		else
 		{
-			_putchar(c);
-			count++;
+			count += _putchar(c);
 		}
 		i++;
 	}
